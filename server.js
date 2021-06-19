3f5fb2b8dc1bdc1964ba0c22d8677acde7152e27
Patrick McBride <pwmcbride@icloud.com>
2021-06-19 19:38:52 -0400

    fixing heroku app

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ const app = express();
 const db = require('./models');
 const Role = db.role;
 
-app.use(express.static(__dirname + '/dist/tarkovmapselector'));
-
 db.mongoose.connect('mongodb+srv://pwmcbrid:out72put@cluster0.lz331.mongodb.net/tarkovUsers?retryWrites=true&w=majority' ,{
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -61,7 +59,8 @@ app.get("/*", function(req, res){
 require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
 
-app.use(cookieParser());
+app.use(express.static(__dirname + '/dist/tarkovmapselector'));
+
 
 // Start the app by listening on the default Heroku port
 
