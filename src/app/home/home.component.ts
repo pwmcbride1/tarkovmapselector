47af7b910facbdb153dd6cef7d3a9dc9f8531696
Patrick McBride <pwmcbride@icloud.com>
2021-06-20 16:37:10 -0400

    finished implementing favoriting

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,8 @@ export class HomeComponent implements OnInit {
 
   myInterval = 100;
 
+  result: any;
+
   currentUser: any;
   username: any;
   favArray: any[] = [];
@@ -46,9 +48,48 @@ export class HomeComponent implements OnInit {
   }
 
   spin(){
-    let result: number;
-    result = this.getRandomInt(7);
-    this.answer = this.mapResult(result);
+    //let result: number;
+    let randoSelection = 7;
+    let mapSelection: string;
+    let randoResult: number;
+    let mapArray = ["Factory", "Woods", "Customs", "Interchange", "Reserve", "Shoreline", "Labs"];
+
+    if(this.token.getToken() != null){
+      this.auth.getInfo(this.username).subscribe(data => {
+        randoSelection += data.favorites.length * 2;
+        for (var i = 0; i < data.favorites.length; i++){
+          mapArray.push(data.favorites[i]);
+          mapArray.push(data.favorites[i]);
+        }
+      })
+    }
+
+    randoResult = this.getRandomInt(randoSelection) - 1;
+    mapSelection = mapArray[randoResult];
+    if(mapSelection == "Factory"){
+      this.result = 0;
+    }
+    if(mapSelection == "Woods"){
+      this.result = 2;
+    }
+    if(mapSelection == "Customs"){
+      this.result = 1;
+    }
+    if(mapSelection == "Interchange"){
+      this.result = 3;
+    }
+    if(mapSelection == "Reserve"){
+      this.result = 5;
+    }
+    if(mapSelection == "Shoreline"){
+      this.result = 6;
+    }
+    if(mapSelection == "Labs"){
+      this.result = 4;
+    }
+
+    console.log(this.result);
+    this.answer = this.mapResult(this.result);
     console.log(this.answer);
     if(this.token.getToken() != null){
       this.auth.updateSpins(this.username).subscribe();
